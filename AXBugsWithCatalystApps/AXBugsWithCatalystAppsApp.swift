b8e1771dc7d7a23be5630635290aa1e174d304c4
Guill Lo <guill.bout@gmail.com>
2021-12-18 23:50:02 +0800

    Initial Commit

---
+++ AXBugsWithCatalystApps/AXBugsWithCatalystAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AXBugsWithCatalystAppsApp.swift
+//  AXBugsWithCatalystApps
+//
+//  Created by Guillaume Leclerc on 18/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AXBugsWithCatalystAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AXBugsWithCatalystApps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AXBugsWithCatalystApps
+//
+//  Created by Guillaume Leclerc on 18/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
